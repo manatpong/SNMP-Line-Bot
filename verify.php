7ba5d3f13352dab20c273f9180eb27afddbf931e
manatpong <582110134@camt.info>
2018-10-31 14:54:53 +0700

    init

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php 
-    $access_token = 'AOoviY4y5C5soMiGpzZHCB5PwJd1phNBCiNd2kFsOsE8cdOu5zrFM4wKGHBDoykrhFFigJe/S1zFJh9ydb7rsLP/BYlDmAe98+YmqmSgMXfyAk4sa8theAEVAqSbKb8Hye/UK4Mrsj8k0WIo7wrSSgdB04t89/1O/w1cDnyilFU=';
+    $access_token = 'FAIQtDdhcZAWWMH6IQHYOwQIrkZVUGZjLbKu3Gq5xWD/qFkyO+rMwQemlXZWJ0WYhFFigJe/S1zFJh9ydb7rsLP/BYlDmAe98+YmqmSgMXfUE/jOSTW0Uo+vbgprQbLbo50J6IgyA4gHcn6zG/qrrgdB04t89/1O/w1cDnyilFU=';
     $url = 'https://api.line.me/v1/oauth/verify';
     $headers = array('Authorization: Bearer ' . $access_token);
     $ch = curl_init($url);
