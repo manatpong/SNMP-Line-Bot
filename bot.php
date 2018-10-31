8723d794861fa8689bdebe048dd8e1bb2a58a1b9
manatpong <582110134@camt.info>
2018-10-31 17:27:56 +0700

    committ

--- bot.php
+++ bot.php
@@ -5,6 +5,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+
 if (!is_null($events['events'])) {	
     // Loop through each event	
     foreach ($events['events'] as $event) {		
@@ -27,7 +28,8 @@ if (!is_null($events['events'])) {
             ];			
             $post = json_encode($data);			
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
-            $ch = curl_init($url);			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
+            $ch = curl_init($url);			
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
---
+++ push.php
@@ -0,0 +1,36 @@
+<?php
+   $accessToken = "FAIQtDdhcZAWWMH6IQHYOwQIrkZVUGZjLbKu3Gq5xWD/qFkyO+rMwQemlXZWJ0WYhFFigJe/S1zFJh9ydb7rsLP/BYlDmAe98+YmqmSgMXfUE/jOSTW0Uo+vbgprQbLbo50J6IgyA4gHcn6zG/qrrgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   $content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   //รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+   //รับ id ของผู้ใช้
+   $id = $arrayJson['events'][0]['source']['userId'];
+   #ตัวอย่าง Message Type "Text + Sticker"
+   if($message == "สวัสดี"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา".$id;
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+   }
+   function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+   exit;
+?>
