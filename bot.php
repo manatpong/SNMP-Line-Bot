1868e18d82c8ece6ea6b75c9f16682471448e584
manatpong <582110134@camt.info>
2018-11-01 16:24:26 +0700

    commit

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
     // Loop through each event	
     foreach ($events['events'] as $event) {		
         // Reply only when message sent is in 'text' format		
-        if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'get_room_id:xenoptics') {			
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'get_room_id=xenoptics') {			
             // Get text sent			
             $text = $event['message']['text'];			
             // Get replyToken			
@@ -41,4 +41,4 @@ if (!is_null($events['events'])) {
         }	
     }
 }
-echo "OK";
+echo "Status OK";
