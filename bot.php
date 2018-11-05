176157a15097eec4a9fc75f33c3fa540ae25ea06
manatpong <582110134@camt.info>
2018-11-05 09:35:37 +0700

    update

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back			
             $messages = [				
                 'type' => 'text',				
-                'text' => 'สวัสดี->'.$id.'<-เปนไง'			
+                'text' => 'สวัสดี->'.json_encode($event).'<-เปนไง'			
             ];			
             // Make a POST Request to Messaging API to reply to sender			
             $url = 'https://api.line.me/v2/bot/message/reply';			
