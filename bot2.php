51e0a3b613f800340655fcb049cf4c59ec1c5aa7
Manatpong <582110134@camt.info>
2020-07-25 14:09:39 +0700

    follow2

--- bot2.php
+++ bot2.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
     // Loop through each event	
     foreach ($events['events'] as $event) {		
         // Reply only when message sent is in 'text' format		
-        if (($event['type'] == 'message' && $event['message']['type'] == 'text') || $event['type'] == 'follow') {			
+        // if (($event['type'] == 'message' && $event['message']['type'] == 'text') || $event['type'] == 'follow') {			
             // Get text sent			
             // $text = 'วิวผีบ้าา';			
             // Get replyToken			
@@ -40,7 +40,7 @@ if (!is_null($events['events'])) {
             $result = curl_exec($ch);			
             curl_close($ch);			
             echo $result . "";		
-        }	
+        // }	
     }
 }
-echo "Status OK follow";
+echo "Status OK follow2";
