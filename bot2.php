26ee53cea96b62e47bc8dd522f80d63e4f8fa2ee
Manatpong <582110134@camt.info>
2020-07-25 13:29:55 +0700

    change msg

--- bot2.php
+++ bot2.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back			
             $messages = [				
                 'type' => 'text',				
-                'text' => 'วิวผีบ้าาา'	
+                'text' => 'ทดสอบข้อความ 111'	
             ];			
             // Make a POST Request to Messaging API to reply to sender			
             $url = 'https://api.line.me/v2/bot/message/reply';			
